a27f6db4b0c47aad31078ff652da2bb1980bfeaa
Abeyrathna <Chathuranga.Abeyrathna@pearson.com>
2019-03-06 00:07:18 +0530

    solution added with comments to the scripts

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+## create the docker image using Dockerfile in the root directory
 docker build -t hello-dropwizard .
+
+## Init the docker swarm in the root folder
 docker swarm init
+
+## Deploy the new stack named hello-dropwizard using the docker-compose.yml
 docker stack deploy --compose-file=docker-compose.yml hello-dropwizard
+
+## List the started containers and replica count
 docker service ls
